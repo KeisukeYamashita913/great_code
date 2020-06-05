04eef4590160dd4ae408f4ee625bef167a4f7e6c
KeisukeYamashita913 <62019125+KeisukeYamashita913@users.noreply.github.com>
2020-06-05 20:34:08 +0900

    Update mian.cpp

--- src/mian.cpp
+++ src/mian.cpp
@@ -5,14 +5,15 @@ int main() {
 int mojisuu
     
 for(mojisuu = 1 ; mojisuu <= 1000; mojisuu ++){
-    if(mojisuu % 3 == 0)
+    if(mojisuu % 3 == 0){
       printf("Fizz"\n);
-    else if(mojisuu % 5 == 0)
+    }else if(mojisuu % 5 == 0){
       printf("Buzz"\n);
-    else if(mojisuu % 3 == 0 && mojisuu % 5 == 0){
+    }else if(mojisuu % 3 == 0 && mojisuu % 5 == 0){
       printf("FizzBuzz"\n);
-    else
+    }else{
       printf("%d\n",i);
+    }
   }
   return 0;
  }
