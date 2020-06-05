4496bd242aa3c94872626d527f1231ad917beafa
KeisukeYamashita913 <62019125+KeisukeYamashita913@users.noreply.github.com>
2020-06-05 20:08:43 +0900

    Update mian.cpp

--- src/mian.cpp
+++ src/mian.cpp
@@ -7,7 +7,7 @@ int mojisuu = 0;
 問題：1から順番に数字を発言するのだけれども、その数字が3で割り切れれはFizzと発言して、5ならBuzz、１５でならFizzBuzzと発言する
 #endif
 int main() {
-for(mojisuu < mojiretsu; mojisuu ++){
+for(mojisuu < 1000; mojisuu ++){
     if(mojisuu % 3 == 0){
       printf("Fizz");
     }else if(mojisuu % 5 == 0){
